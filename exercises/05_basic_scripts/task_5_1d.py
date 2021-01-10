334fe0833a00c1db0c3eab8012db256ba2508c8c
Listradenko <listradenko-denis@yandex.ru>
2021-01-10 23:36:06 +0300

    exercise 5.1c,d and 5.2 complete

--- exercises/05_basic_scripts/task_5_1d.py
+++ exercises/05_basic_scripts/task_5_1d.py
@@ -1,37 +1,17 @@
-# -*- coding: utf-8 -*-
-"""
-Задание 5.1d
-
-Переделать скрипт из задания 5.1c таким образом, чтобы, при запросе параметра,
-пользователь мог вводить название параметра в любом регистре.
-
-Пример выполнения скрипта:
-$ python task_5_1d.py
-Введите имя устройства: r1
-Введите имя параметра (ios, model, vendor, location, ip): IOS
-15.4
-
-
-Ограничение: нельзя изменять словарь london_co.
-
-Все задания надо выполнять используя только пройденные темы.
-То есть эту задачу можно решить без использования условия if.
-"""
-
 london_co = {
     "r1": {
         "location": "21 New Globe Walk",
         "vendor": "Cisco",
         "model": "4451",
         "ios": "15.4",
-        "ip": "10.255.0.1",
+        "ip": "10.255.0.1"
     },
     "r2": {
         "location": "21 New Globe Walk",
         "vendor": "Cisco",
         "model": "4451",
         "ios": "15.4",
-        "ip": "10.255.0.2",
+        "ip": "10.255.0.2"
     },
     "sw1": {
         "location": "21 New Globe Walk",
@@ -40,6 +20,13 @@ london_co = {
         "ios": "3.6.XE",
         "ip": "10.255.0.101",
         "vlans": "10,20,30",
-        "routing": True,
-    },
+        "routing": True
+    }
 }
+dev = input('Enter device name: ')
+k = list(london_co[dev].keys())
+k2 = ','.join(k)
+param = input(f"Input device parametr ({k2}): ")
+print(london_co[dev].get(param.lower(),'Такого параметра нет'))
+#print(london_co[dev][param])
+#print(k)   
--- exercises/05_basic_scripts/task_5_2.py
+++ exercises/05_basic_scripts/task_5_2.py
@@ -1,26 +1,43 @@
-# -*- coding: utf-8 -*-
-"""
-Задание 5.2
+ip_add = input('Введите ip-ceти в формате (10.1.1.0/24) : ')
 
-Запросить у пользователя ввод IP-сети в формате: 10.1.1.0/24
+# действия с ip_адресом
+ip = ip_add[0:ip_add.find('/')]
+ip_list = ip.split('.')
+int_ip = [int(ip_list[0],10), int(ip_list[1],10), int(ip_list[2],10), int(ip_list[3],10)]
 
-Затем вывести информацию о сети и маске в таком формате:
+# действия с маской
+mask = ip_add[ip_add.find('/'):] # первая строка для вывода
+x = int(mask.replace('/',''),10)
+y = (32 - x) # число для уножения на "0"
+bit = ("1"*x)+("0"*y) 
+oktet1 = bit[0:8] # 1 октет маски слева
+oktet2 = bit[8:16] # 2 октет маски
+oktet3 = bit[16:24] # 3 oктет
+oktet4 = bit[24:32] # 4 октет
 
+list_okt10 = [int(oktet1,2), int(oktet2,2), int(oktet3,2), int(oktet4,2)] # create list mask in decimal
+
+#list_okt2 = [bin(list_okt10[0]), bin(list_okt10[1]), bin(list_okt10[2]) ,bin(list_okt10[3])] # create list mask in binary
+
+
+
+print(ip)
+print(f"""
 Network:
-10        1         1         0
-00001010  00000001  00000001  00000000
+{int_ip[0]:<8} {int_ip[1]:<8} {int_ip[2]:<8} {int_ip[3]:<8}
+{int_ip[0]:08b} {int_ip[1]:08b} {int_ip[3]:08b} {int_ip[3]:08b}
 
 Mask:
-/24
-255       255       255       0
-11111111  11111111  11111111  00000000
+{mask}
+{list_okt10[0]:<8} {list_okt10[1]:<8} {list_okt10[2]:<8} {list_okt10[3]:<8}
+{list_okt10[0]:08b} {list_okt10[1]:08b} {list_okt10[2]:08b} {list_okt10[3]:08b}
 
-Проверить работу скрипта на разных комбинациях сеть/маска.
+""")
 
-Подсказка: Получить маску в двоичном формате можно так:
-In [1]: "1" * 28 + "0" * 4
-Out[1]: '11111111111111111111111111110000'
+#print(mask)
+#print(bit)
 
 
-Ограничение: Все задания надо выполнять используя только пройденные темы.
-"""
+
+
+
