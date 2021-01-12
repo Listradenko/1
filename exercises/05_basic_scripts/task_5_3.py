f48fb0095fb67e1004d3856fc4f14949bc3b62a6
Listradenko <listradenko-denis@yandex.ru>
2021-01-12 16:18:44 +0300

    tsk(5.1,5.1a,5.3,5.3a

--- exercises/05_basic_scripts/task_5_3.py
+++ exercises/05_basic_scripts/task_5_3.py
@@ -1,65 +1,44 @@
-# -*- coding: utf-8 -*-
-"""
-Задание 5.3
-
-Скрипт должен запрашивать у пользователя:
-* информацию о режиме интерфейса (access/trunk)
-* номере интерфейса (тип и номер, вида Gi0/3)
-* номер VLANа (для режима trunk будет вводиться список VLANов)
-
-В зависимости от выбранного режима, на стандартный поток вывода,
-должна возвращаться соответствующая конфигурация access или trunk
-(шаблоны команд находятся в списках access_template и trunk_template).
-
-При этом, сначала должна идти строка interface и подставлен номер интерфейса,
-а затем соответствующий шаблон, в который подставлен номер VLANа (или список VLANов).
-
-Ограничение: Все задания надо выполнять используя только пройденные темы.
-То есть эту задачу можно решить без использования условия if и циклов for/while.
-
-Подсказка:
-Подводящим к этому заданию было задание 5.1.
-Чтобы было легче решить это задание, можно посмотреть на задание 5.1 и разобраться как там получилось
-вывести разную информацию в зависимости от ввода пользователя.
-
-Ниже примеры выполнения скрипта, чтобы было проще понять задачу.
-
-Пример выполнения скрипта, при выборе режима access:
-
-$ python task_5_3.py
-Введите режим работы интерфейса (access/trunk): access
-Введите тип и номер интерфейса: Fa0/6
-Введите номер влан(ов): 3
-
-interface Fa0/6
-switchport mode access
-switchport access vlan 3
-switchport nonegotiate
-spanning-tree portfast
-spanning-tree bpduguard enable
-
-Пример выполнения скрипта, при выборе режима trunk:
-$ python task_5_3.py
-Введите режим работы интерфейса (access/trunk): trunk
-Введите тип и номер интерфейса: Fa0/7
-Введите номер влан(ов): 2,3,4,5
-
-interface Fa0/7
-switchport trunk encapsulation dot1q
-switchport mode trunk
-switchport trunk allowed vlan 2,3,4,5
-"""
-
 access_template = [
-    "switchport mode access",
+    "switchport mode access", 
     "switchport access vlan {}",
     "switchport nonegotiate",
     "spanning-tree portfast",
-    "spanning-tree bpduguard enable",
+    "spanning-tree bpduguard enable"
 ]
 
 trunk_template = [
     "switchport trunk encapsulation dot1q",
     "switchport mode trunk",
-    "switchport trunk allowed vlan {}",
+    "switchport trunk allowed vlan {}"
 ]
+
+#x = str(access_template)
+#y = str(trunk_template)
+
+md_inter = input('Введите режим работы интерфеса (access/trunk): ')
+inter = input('Введите тип и номер интерфейса: ')
+vl = input('Введите номер влан(ов): ')
+mode_interface = {
+    "access" :  f"interface {inter} \nswitchport mode access \nswitchport access vlan {vl}\nswitchport nonegotiate\nspanning-tree portfast\nspanning-tree bpduguard enable\n",
+    "trunk"  :  f"interface {inter} \nswitchport trunk encapsulation dot1q \nswitchport mode trunk \nswitchport trunk allowed vlan {vl}\n"
+}
+
+#md_inter = input('Введите режим работы интерфеса (access/trunk): ')
+#inter = input('Введите тип и номер интерфейса: ')
+
+
+
+
+#md_inter = input('Введите режим работы интерфеса (access/trunk): ')
+#inter = input('Введите тип и номер интерфейса: ')
+
+print(mode_interface[md_inter])
+#type_num_int = input('Введите тип и номер интерфейса: ')
+
+#um_vlan = input('Введите номер влан(ов): ')
+
+
+
+
+
+
--- exercises/05_basic_scripts/task_5_3a.py
+++ exercises/05_basic_scripts/task_5_3a.py
@@ -1,26 +1,71 @@
-# -*- coding: utf-8 -*-
-"""
-Задание 5.3a
-
-Дополнить скрипт из задания 5.3 таким образом, чтобы, в зависимости от выбранного режима,
-задавались разные вопросы в запросе о номере VLANа или списка VLANов:
-* для access: 'Введите номер VLAN:'
-* для trunk: 'Введите разрешенные VLANы:'
-
-Ограничение: Все задания надо выполнять используя только пройденные темы.
-То есть эту задачу можно решить без использования условия if и циклов for/while.
-"""
-
 access_template = [
-    "switchport mode access",
+    "switchport mode access", 
     "switchport access vlan {}",
     "switchport nonegotiate",
     "spanning-tree portfast",
-    "spanning-tree bpduguard enable",
+    "spanning-tree bpduguard enable"
 ]
 
 trunk_template = [
     "switchport trunk encapsulation dot1q",
     "switchport mode trunk",
-    "switchport trunk allowed vlan {}",
+    "switchport trunk allowed vlan {}"
 ]
+
+
+
+
+
+
+#inter = 0
+vl = 0
+
+md_inter = input('Введите режим работы интерфеса (access/trunk): ')
+inter = input('Введите тип и номер интерфейса: ')
+mode_interface = {
+    "access" : {
+        "conf" : f"interface {inter} \nswitchport mode access \nswitchport access vlan {vl}\nswitchport nonegotiate\nspanning-tree portfast\nspanning-tree bpduguard enable\n",
+        "text_vla" : "Введите номер VLAN:" 
+    },
+    
+    "trunk" : {
+        "conf1" : f"interface {inter} \nswitchport trunk encapsulation dot1q \nswitchport mode trunk \nswitchport trunk allowed vlan {vl}\n",
+        "text_vlat" : "Введите разрешенные VLANы"    
+    } 
+    
+}
+
+
+#keys = list(mode_interface.keys())
+
+#md_inter = input('Введите режим работы интерфеса (access/trunk): ')
+#inter = input('Введите тип и номер интерфейса: ')
+k = list(mode_interface[md_inter].values()) # список значений словаря
+#mode_interface2 = mode_interface.copy()
+#k2 = list(mode_interface2[md_inter].keys()) # список значений ключей
+vl = input(f'{k[1]} ')
+
+mode_interface2 = {
+    "access" : {
+        "conf" : f"interface {inter} \nswitchport mode access \nswitchport access vlan {vl}\nswitchport nonegotiate\nspanning-tree portfast\nspanning-tree bpduguard enable\n",
+        "text_vla" : "Введите номер VLAN:" 
+    },
+    
+    "trunk" : {
+        "conf1" : f"interface {inter} \nswitchport trunk encapsulation dot1q \nswitchport mode trunk \nswitchport trunk allowed vlan {vl}\n",
+        "text_vlat" : "Введите разрешенные VLANы"    
+    } 
+    
+}
+
+
+k2 = list(mode_interface2[md_inter].keys())
+print(mode_interface2[md_inter][k2[0]])
+
+
+#um_vlan = input('Введите номер влан(ов): ')
+
+
+
+
+
