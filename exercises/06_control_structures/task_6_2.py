3b122c0e8fb2debec1724765cc756913611e099b
Listradenko <listradenko-denis@yandex.ru>
2021-01-18 22:35:37 +0300

    task6.2 complete

--- exercises/06_control_structures/task_6_2.py
+++ exercises/06_control_structures/task_6_2.py
@@ -1,15 +1,16 @@
-# -*- coding: utf-8 -*-
-"""
-Задание 6.2
+ip_add = input('Введите ip-адрес в формате 10.0.1.1  : ')
 
-1. Запросить у пользователя ввод IP-адреса в формате 10.0.1.1
-2. В зависимости от типа адреса (описаны ниже), вывести на стандартный поток вывода:
-   'unicast' - если первый байт в диапазоне 1-223
-   'multicast' - если первый байт в диапазоне 224-239
-   'local broadcast' - если IP-адрес равен 255.255.255.255
-   'unassigned' - если IP-адрес равен 0.0.0.0
-   'unused' - во всех остальных случаях
+ip_list = ip_add.split('.')
 
+int_ip = [int(ip_list[0],10),int(ip_list[1],10),int(ip_list[2],10),int(ip_list[3],10)]
 
-Ограничение: Все задания надо выполнять используя только пройденные темы.
-"""
+if (int_ip[0] > 1 and int_ip[0] < 223):
+    print('unicast')
+elif (int_ip[0] > 224 and int_ip[0] < 239):
+    print('multicast')
+elif (int_ip[0] == 255 and int_ip[1] == 255 and int_ip[2] == 255 and int_ip[3] == 255):
+    print('local broadcast')
+elif (int_ip[0] == 0 and int_ip[1] == 0 and int_ip[2] == 0 and int_ip[3] == 0):
+    print('unassigned')
+else:
+    print('unused')
